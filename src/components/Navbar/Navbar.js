4bc9a98232f90230e03d041e465554c34b72701a
Yanwen <ywwenwang@gmail.com>
2018-03-24 16:46:57 -0700

    Center navbar logo.

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -11,11 +11,6 @@ export const Navbar = () => {
             <Container>
                 <nav className="nav-container d-flex">
                     <Link className="nav__logo text-primary" to="/">The New York Times</Link>
-                    <ul className="nav__group d-flex m-0">
-                        <li>
-                            <Link className="nav__item text-primary" to="/about">Home</Link>
-                        </li>
-                    </ul>
                 </nav>
             </Container>
         </div>
