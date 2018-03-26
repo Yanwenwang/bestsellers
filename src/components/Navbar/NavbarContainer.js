7aeb07a6845338c58851294f5d0c3b5355260d1e
Yanwen <ywwenwang@gmail.com>
2018-03-25 23:46:48 -0700

    Fix lint error.

--- src/components/Navbar/NavbarContainer.js
+++ src/components/Navbar/NavbarContainer.js
@@ -3,10 +3,8 @@ import { connect } from 'react-redux';
 import { toggleLeftNav } from '../../actions/mobileLeftNav';
 import Navbar from './Navbar';
 
-const mapStateToProps = (state) => {};
-
 const mapDispatchToProps = {
     toggleLeftNav
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(Navbar);
+export default connect(null, mapDispatchToProps)(Navbar);
